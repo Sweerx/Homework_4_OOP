d36d619f543dea39ab55d3f92a6c61e9490b45d9
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-08-05 23:48:19 +0300

    create modul and func read for json

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+
+from src.category import Category
+from src.product import Product
+
+
+def get_products_of_json(file: str) -> list:
+    with open(f"../data/{file}", encoding="utf-8") as f:
+        data = json.load(f)
+
+    categories = []
+    for category in data:
+        products = []
+        for product in category["products"]:
+            products.append(Product(**product))
+        category["products"] = products
+        categories.append(Category(**category))
+
+    return categories

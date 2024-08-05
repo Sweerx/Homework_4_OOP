40d5ce929f3a953ee3e8c9859b8eeb9ac359469c
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-08-06 00:26:40 +0300

    add tests for class

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,8 +1,23 @@
 from src.product import Product
 
 
-def test_product_init(first_product: Product) -> None:
-    assert first_product.name == "Iphone 15"
-    assert first_product.description == "Apple Iphone 15"
-    assert first_product.price == 1500
-    assert first_product.quantity == 10
+def test_initialization(product: Product) -> None:
+    assert product.name == "Ноутбук"
+    assert product.description == "Высокопроизводительный ноутбук"
+    assert product.price == 1500.0
+    assert product.quantity == 10
+
+
+def test_price_update(product: Product) -> None:
+    product.price = 1400.0
+    assert product.price == 1400.0
+
+
+def test_quantity_update(product: Product) -> None:
+    product.quantity = 5
+    assert product.quantity == 5
+
+
+def test_description_update(product: Product) -> None:
+    product.description = "Ноутбук средней ценовой категории"
+    assert product.description == "Ноутбук средней ценовой категории"
